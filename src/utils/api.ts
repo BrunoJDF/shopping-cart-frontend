1a0935d9793b82fb88b26646a836e1ea7419bf40
Bruno Josue Diaz Floreano <PE01026487@prosegur.com>
2024-12-01 02:21:55 -0500

    feat: add retry

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,23 +1,23 @@
 import axios from "axios";
+const maxRetries = 3;
+const retryDelay = 1000; // 1 second
 
 export const fetchData = async <T>(url: string): Promise<T> => {
-  try {
-    const response = await axios.get<T>(url);
-    return response.data;
-  } catch (error) {
-    throw new Error(`Error fetching data from ${url}`);
+  for (let attemp = 1; attemp <= maxRetries; attemp++) {
+    try {
+      const response = await axios.get<T>(url);
+      return response.data;
+    } catch (error) {
+      if (attemp < maxRetries) {
+        console.log(`Retrying ${attemp} of ${maxRetries}`);
+        await new Promise((resolve) => setTimeout(resolve, retryDelay));
+      } else {
+        throw new Error(`Error fetching data from ${url} after ${maxRetries} attempts`);
+      }
+    }
   }
+  throw new Error(`Error fetching data from ${url}`);
 };
-
-/*
-export const postData = async <T>(url: string, data: T): Promise<void> => {
-  try {
-    await axios.post(url, data)
-  } catch (error) {
-    throw new Error(`Error posting data to ${url}`)
-  }
-}
-*/
 
 export const postData = async <T, R>(url: string, data: T): Promise<R> => {
   try {

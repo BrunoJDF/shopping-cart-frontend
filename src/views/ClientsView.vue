7d4cf18e3a8e4d96902eb87ad39e5937ba755f78
Bruno Diaz <brunjo12@hotmail.com>
2024-11-30 23:17:33 -0500

    fix: remove api call because crash app

--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -2,27 +2,17 @@
   <div class="clients">
     <h1>Clients</h1>
     <span>{{ msg }}</span>
-    <ul>
-      <li v-for="client in clients" :key="client.id">
-        <router-link :to="{ name: 'ClientView', params: { id: client.id } }">
-          {{ client.name }}
-        </router-link>
-      </li>
-    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import type { Client } from "@/model/client";
-import { getClients } from "@/services/client.service";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "ClientsView",
-  async setup() {
+  setup() {
     const msg = "Welcome to the Clients page!";
-    const clients: Client[] = await getClients();
-    return { msg, clients };
+    return { msg };
   },
 });
 </script>

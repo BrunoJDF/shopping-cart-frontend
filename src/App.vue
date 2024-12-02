75608b58be7944bb07d78b3c73c7b21972120509
Bruno Josue Diaz Floreano <PE01026487@prosegur.com>
2024-12-02 00:49:23 -0500

    feat: add product page

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import { RouterLink, RouterView } from "vue-router";
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/clients">Clients</RouterLink>
+        <RouterLink to="/products">Products</RouterLink>
       </nav>
     </div>
   </header>
